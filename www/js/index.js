765b43d8b9da435bae9cdd9fd8a558a6ca9e6260
nicopons24 <nicopons24@gmail.com>
2015-10-26 20:56:28 +0100

    Creacion BBDD

--- www/js/index.js
+++ www/js/index.js
@@ -1,23 +1,65 @@
+function clickNewPersona() {
+  alert("Pulsado el boton");
+};
 var confDB = {
+  db:"",
   initialize:function() {
     // comprobar si existe la base de datos
     var existDB;
     existDB = window.localStorage.getItem("exist_db");
+    this.db = window.openDatabase("crmDB", "1.0", "miniCRM Database", 2*(1024*1024));
     if (existDB==null) {
-      // pregunta al usuario
-      navigator.notification.alert(
-        "La BBDD no existe",  // mensage
-        this.onConfirm,       // evento de accion
-        "Database",           // titulo
-        "Aceptar"             // opciones
-      );
+      console.log("No existe la BBDD");
+      this.createDB();
     }
   },
 
-  onConfirm:function () {
-    window.localStorage.setItem("exist_db")
-  }
+  createDB:function() {
+    console.log("Creando la BBDD...");
+    this.db.transaction(this.dbCreate, this.dbError, this.dbOk);
+  },
 
+  dbCreate:function(tx) {
+    var juego = "CREATE TABLE IF NOT EXISTS juegos ("+
+    "idjuegos INT NOT NULL,"+
+    "accion BOOLEAN NOT NULL,"+
+    "arcade BOOLEAN NOT NULL,"+
+    "aventuras BOOLEAN NOT NULL,"+
+    "carreras BOOLEAN NOT NULL,"+
+    "casual BOOLEAN NOT NULL,"+
+    "estrategia BOOLEAN NOT NULL,"+
+    "deportes BOOLEAN NOT NULL,"+
+    "puzzles BOOLEAN NOT NULL,"+
+    "rol BOOLEAN NOT NULL,"+
+    "varios BOOLEAN NOT NULL"+
+    ");";
+    var persona = "CREATE TABLE IF NOT EXISTS persona ("+
+    "idpersona INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "+
+    "nombre VARCHAR(32) NOT NULL, "+
+    "apellidos VARCHAR(128) NOT NULL, "+
+    "edad INTEGER NOT NULL, "+
+    "rol VARCHAR(64) NOT NULL, "+
+    "email VARCHAR(64) NOT NULL,"+
+    "CONSTRAINT fk_persona_juegos "+
+    "FOREIGN KEY (idpersona) "+
+    "REFERENCES juegos(idjuegos) "+
+    "ON DELETE NO ACTION "+
+    "ON UPDATE NO ACTION)"
+    ");";
+    console.log(juego);
+    tx.executeSql(juego);
+    console.log(persona);
+    tx.executeSql(persona);
+  },
+
+  dbError:function(e) {
+    console.log("Error al crear BBDD: "+ e.code);
+  },
+
+  dbOk:function() {
+    console.log("BBDD creada correctamente!");
+    window.localStorage.setItem("exist_db", true);
+  }
 };
 
 var app = {
@@ -52,5 +94,6 @@ var app = {
         confDB.initialize();
     }
 };
+
 
 app.initialize();
